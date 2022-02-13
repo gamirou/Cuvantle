e673c05e0025485e921bab472ea24bc1dd40a20f
George Sima <bogdansima20@gmail.com>
2022-02-13 23:59:25 +0000

    Most of the code is done

--- src/index.js
+++ src/index.js
@@ -2,11 +2,57 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import WORDS from './data';
 import reportWebVitals from './reportWebVitals';
+
+// Loader that I can reuse
+// Add loader to html
+// const loader = document.createElement('div');
+// loader.className = 'loader';
+// document.body.appendChild(loader);
+
+// // Define functions
+// const showLoader = () => loader.classList.remove('loader--hide');
+// const hideLoader = () => loader.classList.add('loader--hide');
+// const loadWordFile = () => {
+//   fetch('/cuvinte.txt')
+//     .then((r) => r.text())
+//     .then(text  => {
+//       console.log(text);
+//       const words = text.split('\n');
+//       hideLoader();
+//       ReactDOM.render(
+//         <React.StrictMode>
+//           <App words={words} />
+//         </React.StrictMode>,
+//         document.getElementById('root')
+//       );
+//     })  
+//   }    
+
+
+// showLoader();
+// loadWordFile();
+
+// const loadWordFile = () => {
+//   const xhr = new XMLHttpRequest();
+//   xhr.open('GET', './src/main/words.txt', true);
+//   xhr.onload = () => {
+//     if (xhr.status === 200) {
+//       const words = xhr.responseText.split('\n');
+//       hideLoader();
+//       ReactDOM.render(
+//         <App words={words} />,
+//         document.getElementById('root')
+//       );
+//     }
+//   };
+//   xhr.send();
+// };
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App words={WORDS} />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/main/Cuvantle.js
@@ -0,0 +1,147 @@
+import React, { useCallback, useState, useEffect } from 'react';
+import './Cuvantle.css';
+
+export const Cuvantle = ({ words }) => {
+
+    const generateRandomWord = () => {
+        return words[Math.floor(Math.random() * words.length)].toUpperCase();
+    }
+
+    // const [alphabet, setAlphabet] = useState("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    const [word, setWord] = useState(generateRandomWord());
+    const [cells, setCells] = useState(Array(6).fill('     '));
+    const [cellColours, setCellColours] = useState(Array(30).fill('white'));
+    const [rowIndex, setRowIndex] = useState(0);
+    const [colIndex, setColIndex] = useState(0);
+    const [gameOver, setGameOver] = useState(false);
+
+    const Row = ({ guess }) => {
+        return (
+            <tr>
+                <Cell value={0} guess={guess} />
+                <Cell value={1} guess={guess} />
+                <Cell value={2} guess={guess} />
+                <Cell value={3} guess={guess} />
+                <Cell value={4} guess={guess} />
+            </tr>
+        )
+    }
+
+    const Cell = ({ value, guess }) => {
+        return (
+            <td className={`cell ${cellColours[guess * 5 + value]}`}>{cells[guess][value]}</td>
+        );
+    }
+
+    const handleKeyDown = useCallback((event) => {
+        // console.log(event.keyCode)
+        if (rowIndex === 6) {
+            return;
+        }
+
+        const newCells = [...cells];
+
+        // Backspace
+        if (event.keyCode === 8) {
+            if (colIndex === 0) {
+                return;
+            }
+            newCells[rowIndex] = newCells[rowIndex].substring(0, colIndex - 1);
+            setColIndex(colIndex - 1);
+            setCells(newCells);
+            return;
+        }
+
+        // Enter - submit the word
+        if (event.keyCode === 13 && colIndex === 5) {
+            // Check if word is correct
+            // Change cells colours
+            setCellColours(cellColours.map((colour, index) => {
+                const indexWord = Math.floor(index / 5);
+                if (indexWord !== rowIndex) {
+                    return colour;
+                }
+
+                const indexCol = index % 5;
+                const letter = cells[rowIndex][indexCol];
+                if (word.includes(letter)) {
+                    if (word[indexCol] === letter) {
+                        return 'green';
+                    } else {
+                        return 'orange';
+                    }
+                }
+
+                return colour;
+            }));
+
+            setColIndex(0);
+            setRowIndex(rowIndex + 1);
+
+            // Check if word is correct
+            if (word === newCells[rowIndex]) {
+                console.log("Correct!");
+                setGameOver(true);
+            }
+        }
+
+        // Letters
+        if (event.keyCode >= 65 && event.keyCode <= 90 && !gameOver) {
+            if (colIndex === 5) {
+                return;
+            }
+
+            const letter = String.fromCharCode(event.keyCode);
+            newCells[rowIndex] = newCells[rowIndex].substring(0, colIndex) + letter;
+            setCells(newCells);
+            setColIndex(colIndex + 1);
+        }
+    }, [cells, rowIndex, colIndex, word, cellColours, gameOver]);
+
+    // const handleKeyUp = useCallback((event) => {
+    //     if (!canPress) {
+    //         setCanPress(true);
+    //     }
+    // }, [canPress]);
+
+    useEffect(() => {
+        document.addEventListener('keydown', handleKeyDown);
+        // document.addEventListener('keyup', handleKeyUp);
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+            // document.removeEventListener('keyup', handleKeyUp);
+        }
+    }, [handleKeyDown]);
+
+    console.log(word);
+
+    const restartGame = () => {
+        setWord(generateRandomWord());
+        setCells(Array(6).fill('     '));
+        setCellColours(Array(30).fill('white'));
+        setRowIndex(0);
+        setColIndex(0);
+        setGameOver(false);
+    }
+
+    return (
+        <div className='container'>
+            <h1>Cuvantle</h1>
+
+            <table>
+                <tbody>
+                    <Row guess={0} />
+                    <Row guess={1} />
+                    <Row guess={2} />
+                    <Row guess={3} />
+                    <Row guess={4} />
+                    <Row guess={5} />
+                </tbody>
+            </table>
+
+            <button onClick={restartGame} className={gameOver ? `restart--show` : `restart--hide`}>Restart</button>
+        </div>
+    )
+}
+
+export default Cuvantle;
